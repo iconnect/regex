250b8eee07377c0868295b71e886caa2bd64c306
Chris Dornan <chris@chrisdornan.com>
2016-12-12 14:56:56 +0000

    implement substitutions

--- src/Text/Regex/Simple.hs
+++ src/Text/Regex/Simple.hs
@@ -12,6 +12,8 @@ module Text.Regex.Simple
   , Match(..)
   , substAllCaptures
   , substCaptures
+  , substCaptures'
+  , substCapture
   , captureMatches
   , matchNote
   , matched
@@ -30,12 +32,16 @@ module Text.Regex.Simple
 
 import           Data.Array
 --import qualified Data.IntMap                    as IntMap
+import           Data.List
 import           Data.Maybe
+import           Data.Monoid
+import           Data.Ord
 import           Text.Regex.Base
 
-newtype SubMatches a =
+data SubMatches a =
   SubMatches
-    { _SubMatches :: Array Int (Match a)
+    { _sm_source :: !a
+    , _sm_array  :: !(Array Int (Match a))
     }
   deriving (Show)
 
@@ -48,14 +54,43 @@ data Match a =
     }
   deriving (Show)
 
-substAllCaptures :: Extract a => a -> SubMatches a -> a
-substAllCaptures x = substCaptures $ const $ const x
+substAllCaptures :: (Monoid a,Extract a) => a -> SubMatches a -> a
+substAllCaptures x = substCaptures' $ const $ const x
 
-substCaptures :: Extract a => (Int->a->a) -> SubMatches a -> a
-substCaptures = undefined
+substCaptures :: (Monoid a,Extract a)
+              => (Int->a->a)
+              -> SubMatches a
+              -> a
+substCaptures f = substCaptures' f'
+  where
+    f' 0 = id
+    f' i = f i
+
+substCaptures' :: (Monoid a,Extract a)
+               => (Int->a->a)
+               -> SubMatches a
+               -> a
+substCaptures' f sm =
+    foldr sc _sm_source $ zip [0..] $ left_to_right $ elems _sm_array
+  where
+    sc (i,m) hay   = substCapture (f i) (m { _m_hay = hay })
+
+    SubMatches{..} = sm
+
+substCapture :: (Monoid a,Extract a) => (a->a) -> Match a -> a
+substCapture f m@Match{..} =
+    matchPrefix m <> f _m_needle <> matchSuffix m
 
 captureMatches :: [SubMatches a] -> SubMatches a
-captureMatches = undefined
+captureMatches []          = error "captureMatches: empty list"
+captureMatches sms@(sm0:_) =
+    mk $ left_to_right $ catMaybes $ map matchMay sms
+  where
+    mk as =
+      SubMatches
+        { _sm_source = _sm_source sm0
+        , _sm_array  = listArray (0,length as-1) as
+        }
 
 matchNote :: String -> SubMatches a -> Match a
 matchNote nte sm = fromMaybe oops $ matchMay sm
@@ -72,7 +107,7 @@ subMatches :: SubMatches a -> [Match a]
 subMatches = tailDef [] . subMatches'
 
 subMatches' :: SubMatches a -> [Match a]
-subMatches' = elems . _SubMatches
+subMatches' = elems . _sm_array
 
 (%!) :: SubMatches a -> Int -> Match a
 (%!) = subMatch
@@ -83,8 +118,8 @@ subMatch sm i = fromMaybe oops $ subMatch' sm i
     oops = error $ "subMatch: out of bounds (" ++ show i ++ ")"
 
 subMatch' :: SubMatches a -> Int -> Maybe (Match a)
-subMatch' SubMatches{..} i = case bounds _SubMatches `inRange` i of
-  True  -> Just $ _SubMatches ! i
+subMatch' SubMatches{..} i = case bounds _sm_array `inRange` i of
+  True  -> Just $ _sm_array ! i
   False -> Nothing
 
 matchText :: Match a -> a
@@ -117,7 +152,11 @@ instance RegexContext regex source [MatchText source] =>
       return $ map (cvt s) y
 
 cvt :: source -> MatchText source -> SubMatches source
-cvt hay arr = SubMatches $ fmap f arr
+cvt hay arr =
+    SubMatches
+      { _sm_source = hay
+      , _sm_array  = fmap f arr
+      }
   where
     f (ndl,(off,len)) =
       Match
@@ -130,3 +169,11 @@ cvt hay arr = SubMatches $ fmap f arr
 tailDef :: [a] -> [a] -> [a]
 tailDef df []    = df
 tailDef _  (_:t) = t
+
+-- NB matches and captures from the back end must be listed
+-- left-to-right (i.e., ordered by _m_offset) for this to process
+-- the captures bottom-up; we are not assuming the back ends are
+-- meeting this requirement of regex-base because it is relativly easy
+-- to ensure this critical property
+left_to_right :: [Match a] -> [Match a]
+left_to_right = sortBy $ comparing _m_offset
